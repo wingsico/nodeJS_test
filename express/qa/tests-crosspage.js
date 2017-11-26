2ba61396da031de46ae5d7c86693231c4ad56055
wingsico <wingsico@outlook.com>
2017-11-27 07:37:28 +0800

    grunt + zombie新用法

--- express/qa/tests-crosspage.js
+++ express/qa/tests-crosspage.js
@@ -12,25 +12,27 @@ suite('Cross-Page Tests', () => {
     var referrer = '/tours/hood-river'
     browser.visit(referrer, () => {
       browser.clickLink('.requestGroupRate', () => {
-        assert(browser.field('referrer').value === referrer)
+        assert(browser.resources['0'].request.headers._headers[0][1] === referrer)
       })
+      console.log(browser.resources['0'].request.headers)
       done()      
     })
   })
 
-  test('requesting a group rate from the oregon coast tour page showd populate the referrer field', (done) => {
-    var referrer = '/tours/oregon-coast'
-    browser.visit(referrer, () => {
-      browser.clickLink('.requestGroupRate', () => {
-        assert(browser.field('referrer').value === referrer)
-      })
-      done()      
-    })
-  })
+  // test('requesting a group rate from the oregon coast tour page showd populate the referrer field', (done) => {
+  //   var referrer = '/tours/oregon-coast'
+  //   browser.visit(referrer, () => {
+  //     browser.clickLink('.requestGroupRate', () => {
+  //       assert(browser.field('referrer').value === referrer)
+  //     })
+  //     done()      
+  //   })
+  // })
 
   test('visiting the "request group rate" page dirctly should result in an empty referrer field', (done) => {
     browser.visit('/tours/request-group-rate', () => {
-      assert(browser.field('referrer').value === '')
+      console.log(browser.resources['0'].request.headers._headers)
+      assert(browser.resources['0'].request.headers._headers[0][1] === '')
       done()
     })
   })
